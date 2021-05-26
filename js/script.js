9f7646a697d7f6de97813d1c6f9163c2aa7504c9
SamuelaKC <carnelli.samuela@gmail.com>
2021-05-26 15:49:46 +0200

    modificato Js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-console.log('Prova di Js nel sito, alle righe 89 - 160 - 170');
-document.getElementById('breakTheCircle').innerHTML = 'Rompi il cerchio';
-document.getElementById('herePlaystation').innerHTML = 'Ecco la PlayStation 5';
-document.getElementById('consolePlaystation').innerHTML = 'Console Playstation 5';
-alert ("Ci sono prove di Js, vedere console.log per dettagli!");
+console.log('Prova di Js nel sito, alle righe 89 - 160 - 170'); //frase che si legge solo nel console.log
+document.getElementById('breakTheCircle').innerHTML = 'Rompi il cerchio'; // = sostituisce quello che c'è all'interno dell' h2  riga 89 
+document.getElementById('herePlaystation').innerHTML = 'Ecco la PlayStation 5'; // = sostituisce quello che c'è all'interno dell' h1 riga 160
+document.getElementById('consolePlaystation').innerHTML += ' Playstation 5'; // += aggiunge quello che c'è dopo la scritta riga 170
+
+alert ("Ci sono prove di Js, vedere console.log per dettagli!"); //popup di alert che compare prima di accedere al sito
 
 // ricordarsi di mettere punte e virgola alla fine
