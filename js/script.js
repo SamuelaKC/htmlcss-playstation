e51ebd7adfdcf9b4e0f60bfcb42edd27c583ed2e
SamuelaKC <carnelli.samuela@gmail.com>
2021-05-26 16:04:43 +0200

    modificato file js, aggiunto somma con var

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 console.log('Prova di Js nel sito, alle righe 89 - 160 - 170'); //frase che si legge solo nel console.log
 document.getElementById('breakTheCircle').innerHTML = 'Rompi il cerchio'; // = sostituisce quello che c'è all'interno dell' h2  riga 89 
-document.getElementById('herePlaystation').innerHTML = 'Ecco la PlayStation 5'; // = sostituisce quello che c'è all'interno dell' h1 riga 160
-document.getElementById('consolePlaystation').innerHTML += ' Playstation 5'; // += aggiunge quello che c'è dopo la scritta riga 170
+document.getElementById('herePlaystation').innerHTML += ' PlayStation 5'; // += aggiunge quello che c'è dopo la scritta riga 160
+var x = 2; 
+var y = 3; 
+var sumPlay = x + y; 
+document.getElementById('consolePlaystation').innerHTML = sumPlay; //fa la somma del variabili e scrive il numero riga 170
+
 
 alert ("Ci sono prove di Js, vedere console.log per dettagli!"); //popup di alert che compare prima di accedere al sito
 
