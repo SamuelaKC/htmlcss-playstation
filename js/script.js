31dba0b6a03d74b0cdb6a8250649a9d4ce056f6a
SamuelaKC <carnelli.samuela@gmail.com>
2021-05-26 15:32:56 +0200

    prove di js nelle intestazioni

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('Prova di Js nel sito, alle righe 89 - 160 - 170');
+document.getElementById('breakTheCircle').innerHTML = 'Rompi il cerchio';
+document.getElementById('herePlaystation').innerHTML = 'Ecco la PlayStation 5';
+document.getElementById('consolePlaystation').innerHTML = 'Console Playstation 5';
+alert ("Ci sono prove di Js, vedere console.log per dettagli!");
+
+// ricordarsi di mettere punte e virgola alla fine
